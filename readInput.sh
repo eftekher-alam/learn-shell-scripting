e73f8f93d361934f0920b2c86c24ee276a40e077
eftekher-alam <eftekher@yahoo.com>
2022-11-30 21:48:38 +0600

    Different Type of input

---
+++ readInput.sh
@@ -0,0 +1,17 @@
+# echo "Enter Name : " #With new line
+# read name
+# echo "Your name is $name"
+
+
+# echo -n "Enter Age : " #Without new line
+# read age
+# echo "Your name is $age"
+
+
+# read -p "Home town : " address
+# echo "Your home town is $address"
+
+# Take Array as input
+echo -n "Enter your friends name : "
+read -a friendName
+echo "Your friends are : ${friendName[@]}"
