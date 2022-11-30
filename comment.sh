ba847a98b92faa870301d430a88dd260d482b185
eftekher-alam <eftekher@yahoo.com>
2022-11-30 21:30:10 +0600

    How to write a comment

---
+++ comment.sh
@@ -0,0 +1,6 @@
+# By using hash you can make comment
+echo $BASH #This will show you your system bash name
+echo $BASH_VERSION #This will show you which version of bash is installed.
+
+name="Eftekher"
+echo "My name is" $name
