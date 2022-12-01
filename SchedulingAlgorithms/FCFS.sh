978b8cb6a065daac5989edd402212550ed5be898
eftekher-alam <eftekher@yahoo.com>
2022-12-01 17:11:42 +0600

    FCFS Scheduling Algorithm

---
+++ SchedulingAlgorithms/FCFS.sh
@@ -0,0 +1,12 @@
+echo -n "Enter Brust Time : "
+read -a BT 
+
+WT=(0) #First process wait 0 second
+
+for (( i=0; i<2; i++)) #we have already assigned 0 Waiting time for P1, now just have to find P2 and P3. so loop will fun for two times
+do
+    echo "loop $i"
+    WT[ ${#WT[@]} ]=$(( ${WT[$i]} + ${BT[$i]} )) 
+done
+
+echo ${WT[@]}
